611cb935a762cbf25bff2dd59f363d29c37e81ca
mauricio <mauricio.chaves.chaves@est.una.ac.cr>
2023-11-19 13:28:23 -0600

    funcionalidad con linux

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/AplicacionADN.cpp" "CMakeFiles/untitled1.dir/AplicacionADN.cpp.o" "gcc" "CMakeFiles/untitled1.dir/AplicacionADN.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/ContEnfermedades.cpp" "CMakeFiles/untitled1.dir/ContEnfermedades.cpp.o" "gcc" "CMakeFiles/untitled1.dir/ContEnfermedades.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/ContPacientes.cpp" "CMakeFiles/untitled1.dir/ContPacientes.cpp.o" "gcc" "CMakeFiles/untitled1.dir/ContPacientes.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/Enfermedad.cpp" "CMakeFiles/untitled1.dir/Enfermedad.cpp.o" "gcc" "CMakeFiles/untitled1.dir/Enfermedad.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/IReporte.cpp" "CMakeFiles/untitled1.dir/IReporte.cpp.o" "gcc" "CMakeFiles/untitled1.dir/IReporte.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/InterfazUsuario.cpp" "CMakeFiles/untitled1.dir/InterfazUsuario.cpp.o" "gcc" "CMakeFiles/untitled1.dir/InterfazUsuario.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/Paciente.cpp" "CMakeFiles/untitled1.dir/Paciente.cpp.o" "gcc" "CMakeFiles/untitled1.dir/Paciente.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/RepAnalisis1.cpp" "CMakeFiles/untitled1.dir/RepAnalisis1.cpp.o" "gcc" "CMakeFiles/untitled1.dir/RepAnalisis1.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/RepAnalisis2.cpp" "CMakeFiles/untitled1.dir/RepAnalisis2.cpp.o" "gcc" "CMakeFiles/untitled1.dir/RepAnalisis2.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/RepAnalisis3.cpp" "CMakeFiles/untitled1.dir/RepAnalisis3.cpp.o" "gcc" "CMakeFiles/untitled1.dir/RepAnalisis3.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/main.cpp" "CMakeFiles/untitled1.dir/main.cpp.o" "gcc" "CMakeFiles/untitled1.dir/main.cpp.o.d"
+  "/home/mauricio/CLionProjects/Proyecto2MCAV/utilities.cpp" "CMakeFiles/untitled1.dir/utilities.cpp.o" "gcc" "CMakeFiles/untitled1.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/AplicacionADN.cpp.o"
+  "CMakeFiles/untitled1.dir/AplicacionADN.cpp.o.d"
+  "CMakeFiles/untitled1.dir/ContEnfermedades.cpp.o"
+  "CMakeFiles/untitled1.dir/ContEnfermedades.cpp.o.d"
+  "CMakeFiles/untitled1.dir/ContPacientes.cpp.o"
+  "CMakeFiles/untitled1.dir/ContPacientes.cpp.o.d"
+  "CMakeFiles/untitled1.dir/Enfermedad.cpp.o"
+  "CMakeFiles/untitled1.dir/Enfermedad.cpp.o.d"
+  "CMakeFiles/untitled1.dir/IReporte.cpp.o"
+  "CMakeFiles/untitled1.dir/IReporte.cpp.o.d"
+  "CMakeFiles/untitled1.dir/InterfazUsuario.cpp.o"
+  "CMakeFiles/untitled1.dir/InterfazUsuario.cpp.o.d"
+  "CMakeFiles/untitled1.dir/Paciente.cpp.o"
+  "CMakeFiles/untitled1.dir/Paciente.cpp.o.d"
+  "CMakeFiles/untitled1.dir/RepAnalisis1.cpp.o"
+  "CMakeFiles/untitled1.dir/RepAnalisis1.cpp.o.d"
+  "CMakeFiles/untitled1.dir/RepAnalisis2.cpp.o"
+  "CMakeFiles/untitled1.dir/RepAnalisis2.cpp.o.d"
+  "CMakeFiles/untitled1.dir/RepAnalisis3.cpp.o"
+  "CMakeFiles/untitled1.dir/RepAnalisis3.cpp.o.d"
+  "CMakeFiles/untitled1.dir/main.cpp.o"
+  "CMakeFiles/untitled1.dir/main.cpp.o.d"
+  "CMakeFiles/untitled1.dir/utilities.cpp.o"
+  "CMakeFiles/untitled1.dir/utilities.cpp.o.d"
+  "untitled1"
+  "untitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
